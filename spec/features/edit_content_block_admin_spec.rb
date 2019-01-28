9ffad978dd1e70cff4c843a0e690098983d36554
GriffinJ <jrgriffiniii@gmail.com>
2019-01-28 16:16:01 -0500

    Pruning statements from the edit content block spec

--- spec/features/edit_content_block_admin_spec.rb
+++ spec/features/edit_content_block_admin_spec.rb
@@ -11,52 +11,51 @@ RSpec.describe 'Editing content blocks as admin', :js do
       click_link 'Content Blocks'
     end
 
-    it "does not display a confirmation message when form data has not changed" do
+    it 'displays the default markup' do
       expect(page).to have_content('Content Blocks')
       expect(page).to have_content('Announcement')
-      click_link 'Marketing Text'
-      expect(page).not_to have_content(confirm_modal_text)
-    end
 
-    it "displays a confirmation message when form data has changed" do
-      expect(page).to have_content('Content Blocks')
-      expect(page).to have_content('Announcement')
       expect(page).to have_selector('#content_block_announcement_ifr')
-      within_frame('content_block_announcement_ifr') do
-        find('body').set('Updated text.')
-      end
-      click_link 'Marketing Text'
-      within('#nav-safety-modal') do
-        expect(page).to have_content(confirm_modal_text)
-      end
+
+      expect(page).to have_selector('#announcement_text', class: 'active')
+      expect(page).not_to have_selector('#marketing', class: 'active')
     end
 
-    it "does not change tab when user dismisses the confirmation" do
-      expect(page).to have_selector('#announcement_text', class: 'active')
-      expect(page).not_to have_selector('#marketing', class: 'active')
-      within_frame('content_block_announcement_ifr') do
-        find('body').set('Updated text.')
-      end
-      click_link 'Marketing Text'
-      within('#nav-safety-modal') do
-        click_button('OK')
-      end
-      expect(page).not_to have_selector('#marketing', class: 'active')
-      expect(page).to have_selector('#announcement_text', class: 'active')
-    end
-
-    it "does not redisplay the confirmation unless form data is changed" do
-      expect(page).to have_selector('#announcement_text', class: 'active')
-      expect(page).not_to have_selector('#marketing', class: 'active')
-      within_frame('content_block_announcement_ifr') do
-        find('body').set('Updated text.')
-      end
-      click_link 'Marketing Text'
-      within('#nav-safety-modal') do
-        click_button('OK')
-      end
+    it "does not display a confirmation message when form data has not changed" do
       click_link 'Marketing Text'
       expect(page).not_to have_content(confirm_modal_text)
+    end
+
+    context 'when the form data is changed' do
+      before do
+        within_frame('content_block_announcement_ifr') do
+          find('body').set('Updated text.')
+        end
+
+        click_link 'Marketing Text'
+      end
+
+      it "displays a confirmation message when form data has changed" do
+        within('#nav-safety-modal') do
+          expect(page).to have_content(confirm_modal_text)
+        end
+      end
+
+      it "does not change tab when user dismisses the confirmation" do
+        within('#nav-safety-modal') do
+          click_button('OK')
+        end
+        expect(page).not_to have_selector('#marketing', class: 'active')
+        expect(page).to have_selector('#announcement_text', class: 'active')
+      end
+
+      it "does not redisplay the confirmation unless form data is changed" do
+        within('#nav-safety-modal') do
+          click_button('OK')
+        end
+        click_link 'Marketing Text'
+        expect(page).not_to have_content(confirm_modal_text)
+      end
     end
   end
 end
