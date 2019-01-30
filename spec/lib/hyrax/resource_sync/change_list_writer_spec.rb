834854e38b5921993ca80a439420698a967a6547
GriffinJ <jrgriffiniii@gmail.com>
2019-01-30 13:56:25 -0500

    Trying to get the specs for Hyrax::ResourceSync::ChangeListWriter passing

--- spec/lib/hyrax/resource_sync/change_list_writer_spec.rb
+++ spec/lib/hyrax/resource_sync/change_list_writer_spec.rb
@@ -2,13 +2,21 @@ RSpec.describe Hyrax::ResourceSync::ChangeListWriter, :clean_repo do
   let(:sitemap) { 'http://www.sitemaps.org/schemas/sitemap/0.9' }
   let(:public_collection) { create(:public_collection) }
   let(:public_work) { create(:public_generic_work) }
-  let(:file_set) { create(:file_set, :public) }
+  let(:file_set) do
+    FileSet.create!(read_groups: ['public'])
+  end
   let(:capability_list) { 'http://example.com/capabilityList.xml' }
   let(:xml) { Nokogiri::XML.parse(subject) }
   let(:url_list) { xml.xpath('//x:url', 'x' => sitemap) }
   let(:instance) do
     described_class.new(resource_host: 'example.com',
                         capability_list_url: capability_list)
+  end
+  let(:namespaces) do
+    {
+      'rs' => "http://www.openarchives.org/rs/terms/",
+      'x' => sitemap
+    }
   end
 
   subject { instance.write }
@@ -38,20 +46,19 @@ RSpec.describe Hyrax::ResourceSync::ChangeListWriter, :clean_repo do
       expect(capability).to eq capability_list
 
       expect(location(1)).to eq "http://example.com/concern/file_sets/#{file_set.id}"
-      expect(change(1)).to eq "created"
+      changed_elements = xml.xpath("//x:url[1]/rs:md/@change", namespaces)
+      expect(changed_elements.map(&:value)).to eq ['created']
 
       expect(location(2)).to eq "http://example.com/concern/generic_works/#{public_work.id}"
-      expect(change(2)).to eq "created"
+      changed_elements = xml.xpath("//x:url[2]/rs:md/@change", namespaces)
+      expect(changed_elements.map(&:value)).to eq ['created']
 
       expect(location(3)).to eq "http://example.com/collections/#{public_collection.id}"
-      expect(change(3)).to eq "created"
+      changed_elements = xml.xpath("//x:url[3]/rs:md/@change", namespaces)
+      expect(changed_elements.map(&:value)).to eq ['created']
 
       expect(url_list.count).to eq 3
     end
-  end
-
-  def change(n)
-    query(n, 'rs:md/@change', 'rs' => "http://www.openarchives.org/rs/terms/")
   end
 
   def location(n)
